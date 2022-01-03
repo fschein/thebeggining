002c9473083fdb8ea40cdc8d6293a3f7727b4333
fschein <filipeschein@pm.me>
2022-01-03 15:12:20 -0300

    concatenate

---
+++ cursoPython/aula4/exercicios.py
@@ -0,0 +1,4 @@
+print('Filipe Schein', type('a'))
+print(30, type(1))
+print(1.75, type(1.7))
+print("maior de idade?", 30 > 18, type(30 > 18))
---
+++ cursoPython/aula5/aula5.py
@@ -0,0 +1,21 @@
+nome = 'Filipe Schein'
+idade = 30
+altura = 1.75
+peso = 90
+e_maior = idade > 18
+imc = (peso / altura ** 2)
+
+"""
+print('Nome:', nome)
+print('idade:', idade)
+print('Altura:', altura)
+print('Peso', peso)
+print('E maior?:', e_maior)
+"""
+
+print(nome, 'tem', idade, 'anos de idade, pesa', peso, 'e tem', altura, 'de altura. '
+                                                                        'O IMC de', nome, 'possui o valor de', imc)
+
+
+
+
