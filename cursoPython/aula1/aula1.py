adc58caa720a42287f59ae4279064b3eb1ca3886
fschein <filipeschein@pm.me>
2022-01-02 23:32:54 -0300

    leftovers

---
+++ cursoPython/aula1/aula1.py
@@ -0,0 +1,9 @@
+print('Linha 1')
+print('Linha 2')  # falando do que se trata essa linha - python requer dois espacos!
+"""
+print('Linha 3')
+"""
+
+# Explicando
+print('Linha 4')
+print('Linha 5')
